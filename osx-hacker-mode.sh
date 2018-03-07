79d029f571cf0ee2924589677bada45405293a5f
Matthieu Bousendorfer <edenpulse@gmail.com>
2018-03-07 09:50:18 +0100

    Updates
    Removed some deprecated stuff
    added some terminal tweaks

--- osx-hacker-mode.sh
+++ osx-hacker-mode.sh
@@ -35,16 +35,16 @@ spctl --master-disable
 
 echo ""
 echo "Brewing Stuff"
-brew install mas node subliminal neofetch wget ssh-copy-id mackup htop youtube-dl thefuck
+brew install exa mas node subliminal neofetch wget ssh-copy-id mackup htop youtube-dl thefuck zsh-syntax-highlighting zsh-history-substring-search
 
 echo ""
 echo "Installing Quicklook plugins"
 # https://github.com/sindresorhus/quick-look-plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
 echo ""
 echo "Installing My App Package"
-brew cask install iina monolingual paparazzi github-desktop 1password applepi-baker daisydisk spectacle sitesucker sequel-pro superduper virtualbox virtualbox-extension-pack transmission-remote-gui imageoptim textual bartender alfred sublime-text dropbox cleanmymac iterm2 slack spotify appcleaner firefox-beta fliqlo aerial skype the-unarchiver google-chrome adium calibre flux fontprep handbrake transmission
+brew cask install iina monolingual paparazzi 1password applepi-baker daisydisk spectacle sequel-pro superduper virtualbox virtualbox-extension-pack transmission-remote-gui imageoptim textual bartender alfred sublime-text dropbox cleanmymac iterm2 slack spotify appcleaner fliqlo aerial skype the-unarchiver google-chrome adium calibre flux fontprep handbrake transmission
 
 echo ""
 echo "Restoring Apps Settings from Dropbox with Mackup"
